2b81318d8544f0ee7038bf646b567e6d0f93c938
Paweesuda Pearnvittayavach <paweesuda.p@arise.tech>
2024-05-19 20:52:39 +0700

    level1

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -3,11 +3,23 @@ package main
 import "testing"
 
 func TestFizzBuzzShouldReturn1WhenInput1(t *testing.T) {
+
 	input := 1
 
 	got := FizzBuzz(input)
 
 	want := "1"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
+func TestFizzBuzzShouldReturn1WhenInput2(t *testing.T) {
+
+	input := 2
+
+	got := FizzBuzz(input)
+
+	want := "2"
 	if got != want {
 		t.Errorf("got %q but want %q", got, want)
 	}
