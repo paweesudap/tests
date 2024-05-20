a08115516aeb87c12e5887660d61362fea6ee811
Paweesuda Pearnvittayavach <paweesuda.p@arise.tech>
2024-05-20 09:52:39 +0700

    level1

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -2,7 +2,19 @@ package main
 
 import "strconv"
 
-func FizzBuzz(n int) string {
+func FizzBuzz(n int) any {
+	if n%15 == 0 {
+		return "fizzbuzz"
+	} else if n%3 == 0 {
+		return "fizz"
+	} else if n%5 == 0 {
+		return "buzz"
+	}
+	str := strconv.Itoa(n)
+	return str
+}
+
+func FizzBuzz2(n int) string {
 	fizzBuzz := make(map[int]string)
 	fizzBuzz[3] = "fizz"
 	fizzBuzz[5] = "buzz"
