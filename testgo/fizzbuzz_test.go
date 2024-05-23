af99146a287425f28af7aeaeff0b4201cc75538f
Paweesuda Pearnvittayavach <paweesuda.p@arise.tech>
2024-05-23 15:31:45 +0700

    level3

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -35,3 +35,25 @@ func TestFizzBuzzShouldReturn1WhenInput3(t *testing.T) {
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
+func TestFizzBuzzShouldReturn1WhenInput4(t *testing.T) {
+
+	input := 4
+
+	got := FizzBuzz3(input)
+
+	want := "4"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
+func TestFizzBuzzShouldReturn1WhenInput5(t *testing.T) {
+
+	input := 5
+
+	got := FizzBuzz3(input)
+
+	want := "buzz"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
